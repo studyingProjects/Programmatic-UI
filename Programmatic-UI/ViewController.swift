494d0974e84bf8a0bee698f9869e9cb17e406361
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2023-11-28 16:13:24 +0200

    Initial Commit

---
+++ Programmatic-UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic-UI
+//
+//  Created by Andrei Shpartou on 28/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
